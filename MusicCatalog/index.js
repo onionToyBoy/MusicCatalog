24ac5527c79f29fa6a89b408383899d966eaeb29
onionToyBoy <grazdaninzemlidobri@gmail.com>
2021-06-09 13:55:32 +0300

    Register component has been mooved to navigation folder

--- MusicCatalog/index.js
+++ MusicCatalog/index.js
@@ -1,20 +1,9 @@
 import { Navigation } from 'react-native-navigation';
 
-import { ArtistsScreen } from './src/modules/Artists/ArtistsScreen';
-import { AlbumsScreen } from './src/modules/Albums/AlbumsScreen';
-import { ReduxProvider } from './src/store/ReduxProvider';
 import { RootNavigation } from './src/navigation/RootNavigation';
-import { ArtistsAlbums } from './src/modules/Artists/components/ArtistsAlbums';
-import { AlbumTracks } from './src/modules/Artists/components/AlbumTracks';
-import { routes } from './src/constants/routes';
 
 if (__DEV__) {
 import('./ReactotronConfig').then(() => console.log('Reactotron Configured'));
 }
-
-Navigation.registerComponent(routes.Artists, () => ReduxProvider(ArtistsScreen));
-Navigation.registerComponent(routes.Albums, () => ReduxProvider(AlbumsScreen));
-Navigation.registerComponent(routes.ArtistsAlbums, () => ReduxProvider(ArtistsAlbums));
-Navigation.registerComponent(routes.AlbumTracks, () => ReduxProvider(AlbumTracks));
 
 Navigation.events().registerAppLaunchedListener(RootNavigation());
--- MusicCatalog/src/navigation/RootNavigation.js
+++ MusicCatalog/src/navigation/RootNavigation.js
@@ -1,6 +1,17 @@
 import { Navigation } from 'react-native-navigation';
+
 import { colors } from '../constants/colors';
 import { routes } from '../constants/routes';
+import { ArtistsScreen } from '../modules/Artists/ArtistsScreen';
+import { AlbumsScreen } from '../modules/Albums/AlbumsScreen';
+import { ReduxProvider } from '../store/ReduxProvider';
+import { ArtistsAlbums } from '../modules/Artists/components/ArtistsAlbums';
+import { AlbumTracks } from '../modules/Artists/components/AlbumTracks';
+
+Navigation.registerComponent(routes.Artists, () => ReduxProvider(ArtistsScreen));
+Navigation.registerComponent(routes.Albums, () => ReduxProvider(AlbumsScreen));
+Navigation.registerComponent(routes.ArtistsAlbums, () => ReduxProvider(ArtistsAlbums));
+Navigation.registerComponent(routes.AlbumTracks, () => ReduxProvider(AlbumTracks));
 
 Navigation.setDefaultOptions({
   statusBar: {
