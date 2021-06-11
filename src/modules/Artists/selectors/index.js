08484aaf8137b2a8a917093602084e9b95af060a
onionToyBoy <grazdaninzemlidobri@gmail.com>
2021-06-11 19:44:34 +0300

    Separated logic in store (for tracks) has been created

--- src/modules/Artists/selectors/index.js
+++ src/modules/Artists/selectors/index.js
@@ -2,4 +2,4 @@ export const selectArtists = state => state.artistReducer.artists;
 
 export const selectAlbums = artistId => state => state.albumReducer.albums[artistId];
 
-export const selectTracks = albumId =>  state => state.trackReducer.tracks[albumId];
+export const selectTracks = albumId => state => state.trackReducer.tracks[albumId];
