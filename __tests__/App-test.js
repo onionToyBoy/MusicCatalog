b69c086bded7f4e5f8f87b14a4bb8c7b33d452f3
onionToyBoy <grazdaninzemlidobri@gmail.com>
2021-06-17 16:41:43 +0300

    Return in actions has been removed

--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -4,11 +4,11 @@
 
 import 'react-native';
 import React from 'react';
-import { SearchBar } from '../src/components/SearchBar';
+import App from '../App';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
 it('renders correctly', () => {
-  renderer.create(<SearchBar />);
+  renderer.create(<App />);
 });
--- src/modules/Artists/actions/index.js
+++ src/modules/Artists/actions/index.js
@@ -6,15 +6,13 @@ import {
   SET_ERROR_STATUS,
 } from '../../../constants/actionsTypes';
 
-export const setAlbums = (albums, artistId) => {
-  return {
-    type: SET_ALBUMS,
-    payload: {
-      albums: albums,
-      artistId: artistId,
-    },
-  };
-};
+export const setAlbums = (albums, artistId) => ({
+  type: SET_ALBUMS,
+  payload: {
+    albums: albums,
+    artistId: artistId,
+  },
+});
 
 export const setArtists = artists => {
   return {
@@ -23,26 +21,20 @@ export const setArtists = artists => {
   };
 };
 
-export const setTracks = (tracks, albumId) => {
-  return {
-    type: SET_TRACKS,
-    payload: {
-      tracks: tracks,
-      albumId: albumId,
-    },
-  };
-};
+export const setTracks = (tracks, albumId) => ({
+  type: SET_TRACKS,
+  payload: {
+    tracks: tracks,
+    albumId: albumId,
+  },
+});
 
-export const setLoadingStatus = isLoading => {
-  return {
-    type: SET_LOADING_STATUS,
-    payload: isLoading,
-  };
-};
+export const setLoadingStatus = isLoading => ({
+  type: SET_LOADING_STATUS,
+  payload: isLoading,
+});
 
-export const setErrorStatus = isError => {
-  return {
-    type: SET_ERROR_STATUS,
-    payload: isError,
-  };
-};
+export const setErrorStatus = isError => ({
+  type: SET_ERROR_STATUS,
+  payload: isError,
+});
