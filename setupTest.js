4294f46f1f87d77f9de7ff1ddfad2932b1bb538e
onionToyBoy <grazdaninzemlidobri@gmail.com>
2021-06-28 05:06:12 +0300

    Update setupTest.js

--- setupTest.js
+++ setupTest.js
@@ -1,34 +1,12 @@
-import React from 'react';
-import Enzyme, { configure } from 'enzyme';
+/* eslint-disable */
+
+import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import enableHooks from 'jest-react-hooks-shallow';
 
 enableHooks(jest);
 
-//const React = jest.requireActual('react');
-
 configure({ adapter: new Adapter() });
 
 jest.mock('react-native-navigation', () => ({}));
 jest.mock('@react-native-community/netinfo', () => ({}));
-//jest.mock('react-redux', () => ({useDispatch}));
-
-
-// let hooksEnabled = false;
-
-// const mockedUseEffect = mockUseEffect();
-// const mockedUseLayoutEffect = mockUseEffect();
-// jest.mock('react', () => ({
-//   ...React,
-//   useEffect: (...args) => {
-//     return (hooksEnabled ? mockedUseEffect : React.useEffect)(...args);
-//   },
-//   useLayoutEffect: (...args) => {
-//     return (hooksEnabled ? mockedUseLayoutEffect : React.useLayoutEffect)(...args);
-//   },
-// }));
-// const originalShallow = Enzyme.shallow;
-// Enzyme.shallow = (...args) => {
-//   hooksEnabled = true;
-//   return originalShallow(...args);
-// };
