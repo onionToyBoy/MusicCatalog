9b4a6e7798c58aee12263189529a71a635e148e7
onionToyBoy <grazdaninzemlidobri@gmail.com>
2021-07-08 04:32:48 +0300

    Update setupTest.js

--- setupTest.js
+++ setupTest.js
@@ -1,4 +1,4 @@
-/* eslint-disable max-lines:["error", { "code": 120 }] */
+/* eslint-disable max-lines:["error", "'jest' is not defined"] */
 
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
